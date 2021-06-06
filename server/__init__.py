39010a87f63b0aaf1fefeb652117c19f438466cd
hd-prajwalgowda <hdprajwalgowda@gmail.com>
2021-06-06 19:45:23 +0530

    adds cors and code styling, formating packages

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from pymongo import MongoClient
 import os
 from os.path import join, dirname
@@ -8,6 +9,8 @@ load_dotenv(find_dotenv())
 BACKEND_URL = os.environ.get("BACKEND_URL")
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 client = MongoClient(BACKEND_URL)
 db = client['videosum']
