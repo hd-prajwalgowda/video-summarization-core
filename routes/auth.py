b123262077ed849690e63710396d10c4e527a781
hd-prajwalgowda <hdprajwalgowda@gmail.com>
2021-06-14 20:22:40 +0530

    Adds refresh token logic and increases exp duration for tokens

--- routes/auth.py
+++ routes/auth.py
@@ -2,7 +2,7 @@ from flask import Flask,request,Response,Blueprint
 from pymongo import MongoClient
 import json
 import bcrypt
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, JWTManager
+from flask_jwt_extended import create_refresh_token, create_access_token, jwt_required, get_jwt_identity, JWTManager
 from modules.db import DB
 
 db = DB()
@@ -24,9 +24,9 @@ def user_sign_up():
     pw_hash = bcrypt.hashpw(user_password.encode('utf8'), bcrypt.gensalt())
     user_input = {'name': user_name, 'email': user_email, 'password': pw_hash}
     userInsert = users.insert_one(user_input)
-    # print(userInsert.inserted_id)
-    access_token = create_access_token(identity=user_email)
-    return Response(json.dumps({'login': True, 'msg': 'User created', 'success': True, 'token': access_token})), 200
+    access_token = create_access_token(identity=user_email, fresh=True)
+    refresh_token = create_refresh_token(identity=user_email)
+    return Response(json.dumps({'login': True, 'msg': 'User created', 'success': True,'user':{'name':user_name,'email':user_email}, 'access_token': access_token,'refresh_token':refresh_token})), 200
 
 
 @auth_handler.route('/login', methods=['POST'])
@@ -41,13 +41,24 @@ def user_login():
             passwd = bcrypt.checkpw(user_password.encode(
                 'utf8'), user_data['password'])
             if (passwd):
-                access_token = create_access_token(identity=user_email)
-                return Response(json.dumps({'login': True, 'msg': 'user logged in', 'success': True, 'token': access_token})), 200
+                access_token = create_access_token(identity=user_email, fresh=True)
+                refresh_token = create_refresh_token(identity=user_email)
+                return Response(json.dumps({'login': True, 'msg': 'User Logged in', 'success': True,'user':{'name':user_data['name'],'email':user_data['email']}, 'access_token': access_token, 'refresh_token':refresh_token})), 200
             else:
                 return Response(json.dumps({'login': False, 'msg': 'user password incorrect'})), 403
         except:
-            return Response(json.dumps({'err': 'User data not found'})), 401
+            return Response(json.dumps({'err': 'Unauthorized access'})), 401
     except:
         return Response(json.dumps({'err': 'Internal server error'})), 500
+
+
+@auth_handler.route("/refresh", methods=["POST"])
+@jwt_required(refresh=True)
+def refresh():
+    identity = get_jwt_identity()
+    access_token = create_access_token(identity=identity)
+    return Response(json.dumps({'access_token':access_token})),200
+
+
 
 
